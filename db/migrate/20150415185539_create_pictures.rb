1c88e1e1683bc664dc2988380cb2d939a1ffb86f
Wajid Kagzi <wjkagzi@gmail.com>
2015-04-15 14:58:22 -0400

    Created Picture model and migrated to database.

---
+++ db/migrate/20150415185539_create_pictures.rb
@@ -0,0 +1,12 @@
+class CreatePictures < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.string :artist
+      t.string :string
+      t.string :title
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
